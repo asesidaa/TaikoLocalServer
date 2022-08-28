8b2ddbafbfe0a3e26de567a3a487c429c73e2880
asesidaa <1061472754@qq.com>
2022-08-28 13:47:59 +0800

    Add missing migration

---
+++ TaikoLocalServer/Migrations/20220827163841_FirstMigration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaikoLocalServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<uint>(
+                name: "SongNumber",
+                table: "SongPlayData",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0u);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SongNumber",
+                table: "SongPlayData");
+        }
+    }
+}
