4a25b3a831e3e1ac83221f56a3d0d573605d5a3a
asesidaa <1061472754@qq.com>
2022-09-06 22:32:36 +0800

    Add delete api
    Add delete dialog

---
+++ TaikoLocalServer/Controllers/Api/CardsController.cs
@@ -0,0 +1,30 @@
+﻿namespace TaikoLocalServer.Controllers.Api;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CardsController : BaseController<CardsController>
+{
+    private readonly TaikoDbContext context;
+
+    public CardsController(TaikoDbContext context)
+    {
+        this.context = context;
+    }
+
+    [HttpDelete("{accessCode}")]
+    public async Task<IActionResult> DeleteUser(string accessCode)
+    {
+        var card = await context.Cards.FindAsync(accessCode);
+
+        if (card is null)
+        {
+            return NotFound();
+        }
+
+        context.Cards.Remove(card);
+        await context.SaveChangesAsync();
+
+        return NoContent();
+    }
+    
+}
