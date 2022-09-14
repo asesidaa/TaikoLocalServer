92068c8d2c9f18a0aa145b59d975c91c73335d9f
asesidaa <1061472754@qq.com>
2022-09-14 21:19:55 +0800

    Clean up

--- TaikoLocalServer/Common/Constants.cs
+++ TaikoLocalServer/Common/Constants.cs
@@ -6,8 +6,6 @@ public static class Constants
 
     public const int MUSIC_ID_MAX = 1600;
 
-    public const int MUSIC_FLAG_ARRAY_SIZE = MUSIC_ID_MAX / 8 + 1;
-
     public const int CROWN_FLAG_ARRAY_SIZE = MUSIC_ID_MAX + 1;
     
     public const int DONDAFUL_CROWN_FLAG_ARRAY_SIZE = MUSIC_ID_MAX + 1;
@@ -28,15 +26,23 @@ public static class Constants
 
     public const int MIN_DAN_ID = 1;
     public const int MAX_DAN_ID = 19;
-    public const int GOT_DAN_BITS = MAX_DAN_ID * 4;
 
     public const int TONE_UID_MAX = 19;
-    public const int TITLE_UID_MAX = 814;
-    public const int COSTUME_FLAG_1_ARRAY_SIZE = 154;
-    public const int COSTUME_FLAG_2_ARRAY_SIZE = 140;
-    public const int COSTUME_FLAG_3_ARRAY_SIZE = 156;
-    public const int COSTUME_FLAG_4_ARRAY_SIZE = 58;
-    public const int COSTUME_FLAG_5_ARRAY_SIZE = 129;
     
-    public static readonly int[] CostumeFlagArraySizes = {154, 140, 156, 58, 129};
+    public const int TITLE_UID_MAX = 814;
+
+    private const int COSTUME_FLAG_1_ARRAY_SIZE = 154;
+    private const int COSTUME_FLAG_2_ARRAY_SIZE = 140;
+    private const int COSTUME_FLAG_3_ARRAY_SIZE = 156;
+    private const int COSTUME_FLAG_4_ARRAY_SIZE = 58;
+    private const int COSTUME_FLAG_5_ARRAY_SIZE = 129;
+    public static readonly int[] CostumeFlagArraySizes = 
+    {
+        COSTUME_FLAG_1_ARRAY_SIZE, 
+        COSTUME_FLAG_2_ARRAY_SIZE, 
+        COSTUME_FLAG_3_ARRAY_SIZE, 
+        COSTUME_FLAG_4_ARRAY_SIZE, 
+        COSTUME_FLAG_5_ARRAY_SIZE
+        
+    };
 }
