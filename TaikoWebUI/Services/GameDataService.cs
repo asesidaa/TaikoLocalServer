36a62acfcd14cd8893f100946243c25da7a4c797
shiibe <beckchop9@gmail.com>
2022-09-14 18:22:20 -0400

    Fix maximums

--- TaikoWebUI/Services/GameDataService.cs
+++ TaikoWebUI/Services/GameDataService.cs
@@ -14,12 +14,12 @@ public class GameDataService : IGameDataService
     private ImmutableDictionary<uint, DanData> danMap = null!;
 
     public const int COSTUME_HEAD_MAX = 140;
-    public const int COSTUME_FACE_MAX = 59;
+    public const int COSTUME_FACE_MAX = 58;
     public const int COSTUME_BODY_MAX = 156;
     public const int COSTUME_KIGURUMI_MAX = 154;
     public const int COSTUME_PUCHI_MAX = 129;
     public const int COSTUME_COLOR_MAX = 63;
-    public const int PLAYER_TITLE_MAX = 749;
+    public const int PLAYER_TITLE_MAX = 750;
 
     public static string[] headMap = new string[COSTUME_HEAD_MAX];
     public static string[] faceMap = new string[COSTUME_FACE_MAX];
