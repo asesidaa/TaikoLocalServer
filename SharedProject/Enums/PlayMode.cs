b803cbc67eaf38d7d352b53bfa603dd3202d3683
asesidaa <1061472754@qq.com>
2022-09-18 02:06:11 +0800

    Add initial ai battle

--- SharedProject/Enums/PlayMode.cs
+++ SharedProject/Enums/PlayMode.cs
@@ -5,5 +5,5 @@ public enum PlayMode
     Normal = 0,
     DanMode = 1,
     // Not sure about this
-    AiBattle = 2
+    AiBattle = 6
 }
---
+++ TaikoLocalServer/Entities/AiSectionScoreDatum.cs
@@ -0,0 +1,28 @@
+﻿namespace TaikoLocalServer.Entities;
+
+public class AiSectionScoreDatum
+{
+    public uint Baid { get; set; }
+    
+    public uint SongId { get; set; }
+    
+    public Difficulty Difficulty { get; set; }
+    
+    public int SectionIndex { get; set; }
+    
+    public CrownType Crown { get; set; }
+
+    public bool IsWin { get; set; }
+    
+    public uint Score { get; set; }
+    
+    public uint GoodCount { get; set; }
+    
+    public uint OkCount { get; set; }
+    
+    public uint MissCount { get; set; }
+
+    public uint DrumrollCount { get; set; }
+
+    public AiScoreDatum Parent { get; set; } = null!;
+}
