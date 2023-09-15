e8b26e9352d4a6bfe538e0d2568a33f04f7cd99d
esuo1198 <54134.es@gmail.com>
2023-09-16 02:27:44 +0900

    Correct initial value of UserDatum.CostumeData

--- GameDatabase/Entities/UserDatum.cs
+++ GameDatabase/Entities/UserDatum.cs
@@ -2,33 +2,33 @@
 
 namespace GameDatabase.Entities
 {
-    public partial class UserDatum
-    {
-        public ulong Baid { get; set; }
-        public string MyDonName { get; set; } = string.Empty;
-        public string Title { get; set; } = string.Empty;
-        public uint TitlePlateId { get; set; }
-        public string FavoriteSongsArray { get; set; } = "[]";
-        public string ToneFlgArray { get; set; } = "[]";
-        public string TitleFlgArray { get; set; } = "[]";
-        public string CostumeFlgArray { get; set; } = "[[],[],[],[],[]]";
-        public string GenericInfoFlgArray { get; set; } = "[]";
-        public short OptionSetting { get; set; }
-        public int NotesPosition { get; set; }
-        public bool IsVoiceOn { get; set; }
-        public bool IsSkipOn { get; set; }
-        public uint SelectedToneId { get; set; }
-        public DateTime LastPlayDatetime { get; set; }
-        public uint LastPlayMode { get; set; }
-        public uint ColorBody { get; set; }
-        public uint ColorFace { get; set; }
-        public uint ColorLimb { get; set; }
-        public string CostumeData { get; set; } = "[[],[],[],[],[]]";
-        public bool DisplayDan { get; set; }
-        public bool DisplayAchievement { get; set; }
-        public Difficulty AchievementDisplayDifficulty { get; set; }
+	public partial class UserDatum
+	{
+		public ulong Baid { get; set; }
+		public string MyDonName { get; set; } = string.Empty;
+		public string Title { get; set; } = string.Empty;
+		public uint TitlePlateId { get; set; }
+		public string FavoriteSongsArray { get; set; } = "[]";
+		public string ToneFlgArray { get; set; } = "[]";
+		public string TitleFlgArray { get; set; } = "[]";
+		public string CostumeFlgArray { get; set; } = "[[],[],[],[],[]]";
+		public string GenericInfoFlgArray { get; set; } = "[]";
+		public short OptionSetting { get; set; }
+		public int NotesPosition { get; set; }
+		public bool IsVoiceOn { get; set; }
+		public bool IsSkipOn { get; set; }
+		public uint SelectedToneId { get; set; }
+		public DateTime LastPlayDatetime { get; set; }
+		public uint LastPlayMode { get; set; }
+		public uint ColorBody { get; set; }
+		public uint ColorFace { get; set; }
+		public uint ColorLimb { get; set; }
+		public string CostumeData { get; set; } = "[]";
+		public bool DisplayDan { get; set; }
+		public bool DisplayAchievement { get; set; }
+		public Difficulty AchievementDisplayDifficulty { get; set; }
 
-        public int AiWinCount { get; set; }
-        public virtual Card? Ba { get; set; }
-    }
+		public int AiWinCount { get; set; }
+		public virtual Card? Ba { get; set; }
+	}
 }
