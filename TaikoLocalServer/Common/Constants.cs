27fa2f1668469c2a5664658ade2eea2aa754ba3d
esuo1198 <54134.es@gmail.com>
2023-10-16 21:46:22 +0900

    Fix MUSIC_ID_MAX value

--- TaikoLocalServer/Common/Constants.cs
+++ TaikoLocalServer/Common/Constants.cs
@@ -4,7 +4,7 @@ public static class Constants
 {
 	public const string DATE_TIME_FORMAT = "yyyyMMddHHmmss";
 
-	public const int MUSIC_ID_MAX = 1599;
+	public const int MUSIC_ID_MAX = 1600;
 
 	public const int MUSIC_ID_MAX_EXPANDED = 9000;
 
