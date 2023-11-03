206360b1850ab1bba676e991bc5a144bcfb787f7
Farewell_ <norththelittlefox@gmail.com>
2023-11-03 16:54:56 +0100

    Achievement Board now has numbers on it
    Placement of each score has been done, It still doesn't update properly but the values are properly set. Just need to figure out a way to rerender the scoreboard

--- TaikoWebUI/wwwroot/js/updateTextFit.js
+++ TaikoWebUI/wwwroot/js/updateTextFit.js
@@ -2,10 +2,18 @@
 var title;
 var myDonName;
 var myDonNameOutline;
+var scoreboard;
 var init = false
-const observer = new ResizeObserver(handleResize);
-function handleResize() {
-    updateFit()
+
+const nameplateObserver = new ResizeObserver(handleNameplateResize);
+const scoreboardObserver = new ResizeObserver(handleScoreboardResize);
+
+function handleNameplateResize() {
+    updateNameplate();
+}
+
+function handleScoreboardResize() {
+    updateScoreboard();
 }
 
 function waitForElm(selector) {
@@ -33,7 +41,8 @@ var pushState = history.pushState;
 history.pushState = function () {
     pushState.apply(history, arguments);
     init = false;
-    observer.disconnect();
+    nameplateObserver.disconnect();
+    scoreboardObserver.disconnect();
 };
 
 // Fetches all the relevant objects on the page so updateFit can reference them later
@@ -45,7 +54,7 @@ function initNameplate() {
                     myDonName = elm
                     waitForElm('#nameplate-name-outline').then((elm) => {
                         myDonNameOutline = elm
-                        observer.observe(document.getElementById('nameplate'));
+                        nameplateObserver.observe(document.getElementById('nameplate'));
                         init = true
                     });
                 });
@@ -53,7 +62,16 @@ function initNameplate() {
     }
 }
 
-function updateFit() {
+function initScoreboard() {
+    if (window.location.href.indexOf("Profile") > -1) {
+        waitForElm('#scoreboard').then((elm) => {
+            scoreboard = elm
+            scoreboardObserver.observe(elm);
+        });
+    }
+}
+
+function updateNameplate() {
     if (title.offsetWidth > 0 && title.offsetHeight > 0) {
         textFit(title, { alignHoriz: true, alignVert: true });
         textFit(myDonName, { alignHoriz: true, alignVert: true });
@@ -61,23 +79,40 @@ function updateFit() {
     }
 }
 
+function updateScoreboard() {
+    if (scoreboard.offsetWidth > 0 && scoreboard.offsetHeight > 0) {
+        var row = scoreboard.children;
+        for (var i = 0; i < row.length; i++) {
+            var column = row[i].children;
+            for (var j = 0; j < column.length; j++) {
+                var texts = column[j].children;
+                for (var k = 0; k < texts.length; k++) {
+                    if (texts[k].offsetWidth > 0 && texts[k].offsetHeight > 0) {
+                        textFit(texts[k], { alignHoriz: true, alignVert: true });
+                    }
+                }
+            }
+        }
+    }
+}
 
 // Used to individually update texts on the nameplate
 function updateMyDonName(elm) {
     if (init) {
         myDonName.textContent = elm
         myDonNameOutline.textContent = elm
-        updateFit()
+        updateNameplate()
     }
 }
 function updateTitle(elm) {
     if (init) {
         title.textContent = elm
-        updateFit()
+        updateNameplate()
     }
 }
 
 // Called by html onload when the image of the nameplate is loaded (this ensures the first fit is properly done)
 function nameplateLoaded() {
-    initNameplate()
+    initNameplate();
+    initScoreboard();
 }
