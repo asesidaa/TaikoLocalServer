cbe145cfa351f1eeac52153ae8fc0e13ee1a7b32
KIT! <norththelittlefox@gmail.com>
2023-12-11 17:30:47 +0100

    Fixed scoreboard text update issue
    The scoreboard now properly updates when a new difficulty is selected.
    
    Setting it to "Difficulty.None" defaults to the highest difficulty the player currently has Ranks / Crowns for.
    
    Also removed a bunch of css from the profile.razor file and moved it to a style in app.css.

--- TaikoWebUI/wwwroot/js/updateTextFit.js
+++ TaikoWebUI/wwwroot/js/updateTextFit.js
@@ -9,11 +9,11 @@ const nameplateObserver = new ResizeObserver(handleNameplateResize);
 const scoreboardObserver = new ResizeObserver(handleScoreboardResize);
 
 function handleNameplateResize() {
-    updateNameplate();
+    fitNameplate();
 }
 
 function handleScoreboardResize() {
-    updateScoreboard();
+    fitScoreboard();
 }
 
 function waitForElm(selector) {
@@ -50,14 +50,14 @@ function initNameplate() {
     if (window.location.href.indexOf("Profile") > -1) {
         waitForElm('#nameplate-title').then((elm) => {
             title = elm
-                waitForElm('#nameplate-name').then((elm) => {
-                    myDonName = elm
-                    waitForElm('#nameplate-name-outline').then((elm) => {
-                        myDonNameOutline = elm
-                        nameplateObserver.observe(document.getElementById('nameplate'));
-                        init = true
-                    });
+            waitForElm('#nameplate-name').then((elm) => {
+                myDonName = elm
+                waitForElm('#nameplate-name-outline').then((elm) => {
+                    myDonNameOutline = elm
+                    nameplateObserver.observe(document.getElementById('nameplate'));
+                    init = true
                 });
+            });
         });
     }
 }
@@ -71,7 +71,7 @@ function initScoreboard() {
     }
 }
 
-function updateNameplate() {
+function fitNameplate() {
     if (title.offsetWidth > 0 && title.offsetHeight > 0) {
         textFit(title, { alignHoriz: true, alignVert: true });
         textFit(myDonName, { alignHoriz: true, alignVert: true });
@@ -79,7 +79,7 @@ function updateNameplate() {
     }
 }
 
-function updateScoreboard() {
+function fitScoreboard() {
     if (scoreboard.offsetWidth > 0 && scoreboard.offsetHeight > 0) {
         var row = scoreboard.children;
         for (var i = 0; i < row.length; i++) {
@@ -97,17 +97,37 @@ function updateScoreboard() {
 }
 
 // Used to individually update texts on the nameplate
-function updateMyDonName(elm) {
+function updateMyDonNameText(elm) {
     if (init) {
         myDonName.textContent = elm
         myDonNameOutline.textContent = elm
-        updateNameplate()
+        fitNameplate()
     }
 }
-function updateTitle(elm) {
+function updateTitleText(elm) {
     if (init) {
         title.textContent = elm
-        updateNameplate()
+        fitNameplate()
+    }
+}
+
+function updateScoreboardText(elm) {
+    if (init) {
+        index = 0;
+        var row = scoreboard.children;
+        for (var i = 0; i < row.length; i++) {
+            var column = row[i].children;
+            for (var j = 0; j < column.length; j++) {
+                var texts = column[j].children;
+                for (var k = 0; k < texts.length; k++) {
+                    if (texts[k].offsetWidth > 0 && texts[k].offsetHeight > 0) {
+                        texts[k].textContent = elm[index];
+                    }
+                }
+                index++;
+            }
+        }
+        fitScoreboard()
     }
 }
 
