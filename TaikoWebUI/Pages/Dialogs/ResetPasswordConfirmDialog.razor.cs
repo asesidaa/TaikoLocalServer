98c7c716f5559c3828165abcac297bc7fc617fd1
S-Sebb <794194678@qq.com>
2023-12-19 00:12:48 +0000

    Implemented reset password button if logged in as admin user

---
+++ TaikoWebUI/Pages/Dialogs/ResetPasswordConfirmDialog.razor.cs
@@ -0,0 +1,34 @@
+﻿namespace TaikoWebUI.Pages.Dialogs;
+
+public partial class ResetPasswordConfirmDialog
+{
+    
+    [CascadingParameter]
+    MudDialogInstance MudDialog { get; set; } = null!;
+
+    [Parameter]
+    public User User { get; set; } = new();
+
+    private void Cancel() => MudDialog.Cancel();
+    
+    private async Task ResetPassword()
+    {
+        var request = new SetPasswordRequest
+        {
+            Baid = User.Baid,
+            Password = "",
+            Salt = ""
+        };
+        var responseMessage = await Client.PostAsJsonAsync("api/Credentials", request);
+
+        if (!responseMessage.IsSuccessStatusCode)
+        {
+            Snackbar.Add("Something went wrong when resetting password!", Severity.Error);
+            MudDialog.Close(DialogResult.Ok(false));
+            return;
+        }
+        
+        Snackbar.Add("Reset password success!", Severity.Success);
+        MudDialog.Close(DialogResult.Ok(true));
+    }
+}
