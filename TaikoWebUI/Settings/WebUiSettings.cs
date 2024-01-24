ccd3b22b41460f073b5c230d760db34afab9ca18
asesidaa <1061472754@qq.com>
2024-01-24 11:12:15 +0800

    Add support for zh-hant Refactor to make language specifiable in appsettings

--- TaikoWebUI/Settings/WebUiSettings.cs
+++ TaikoWebUI/Settings/WebUiSettings.cs
@@ -2,10 +2,11 @@
 
 public class WebUiSettings
 {
-    public bool LoginRequired { get; set; }
-    public bool OnlyAdmin { get; set; }
-    public int BoundAccessCodeUpperLimit { get; set; }
-    public bool RegisterWithLastPlayTime { get; set; }
-    public bool AllowUserDelete { get; set; }
-    public bool AllowFreeProfileEditing { get; set; }
+    public bool       LoginRequired             { get; set; }
+    public bool       OnlyAdmin                 { get; set; }
+    public int        BoundAccessCodeUpperLimit { get; set; }
+    public bool       RegisterWithLastPlayTime  { get; set; }
+    public bool       AllowUserDelete           { get; set; }
+    public bool       AllowFreeProfileEditing   { get; set; }
+    public Language[] SupportedLanguages        { get; set; } = Array.Empty<Language>();
 }
