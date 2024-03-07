10547a835c85458ce09663b1a6b555b43b520426
esuo1198 <54134.es@gmail.com>
2024-03-07 22:50:39 +0900

    Add garmc handle
    Thanks @S-Sebb??

---
+++ TaikoLocalServer/Controllers/Garmc/RegisterSystemBoardController.cs
@@ -0,0 +1,24 @@
+﻿using Garm;
+
+namespace TaikoLocalServer.Controllers.Garmc;
+
+[Route("/v1/s12-jp-dev/garm.SystemBoard/RegisterSystemBoard")]
+[ApiController]
+public class RegisterSystemBoardController : BaseController<RegisterSystemBoardController>
+{
+    [HttpPost]
+    [Produces("application/protobuf")]
+    public async Task<RegisterSystemBoardResponse> RegisterSystemBoard()
+    {
+        HttpContext.Request.EnableBuffering();
+        var body = await HttpContext.Request.BodyReader.ReadAsync();
+        var request = Serializer.Deserialize<RegisterSystemBoardRequest>(body.Buffer);
+        Logger.LogInformation("RegisterSystemBoard request: {Request}", request.Stringify());
+        var response = new RegisterSystemBoardResponse
+        {
+            Token = "114514"
+        };
+        Response.Headers.Append("x-drpc-code", "0");
+        return response;
+    }
+}
---
+++ TaikoLocalServer/Controllers/MuchaActivation/SignatureController.cs
@@ -0,0 +1,43 @@
+﻿using System.Buffers;
+using Microsoft.Extensions.Options;
+using TaikoLocalServer.Settings;
+using System.Text.Json;
+using System.Text;
+
+using System.Text.Json.Serialization;
+
+namespace TaikoLocalServer.Controllers.MuchaActivation;
+
+
+public class SignatureResponse
+{
+    [JsonPropertyName("status")]
+    public int Status { get; set; }
+    
+    [JsonPropertyName("message")]
+    public string? Message { get; set; }
+    
+    [JsonPropertyName("signature")]
+    public string? Signature { get; set; }
+}
+
+[Route("/mucha_activation/signature")]
+[ApiController]
+public class SignatureController : BaseController<SignatureController>
+{
+    [HttpPost]
+    public async Task<SignatureResponse> Signature()
+    {
+        HttpContext.Request.EnableBuffering();
+        var body = await HttpContext.Request.BodyReader.ReadAsync();
+        var json = Encoding.UTF8.GetString(body.Buffer.ToArray());
+        Logger.LogInformation("Signature request: {Request}", json);
+        var response = new SignatureResponse
+        {
+            Status = 200,
+            Message = "Success",
+            Signature = "1220"
+        };
+        return response;
+    }
+}
