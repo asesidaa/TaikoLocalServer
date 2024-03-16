1514fd16fdb0a1263e7e9374bdee0d0f1c080127
asesidaa <1061472754@qq.com>
2024-03-16 22:53:05 +0800

    Cleanup services

--- TaikoLocalServer/Services/DanScoreDatumService.cs
+++ TaikoLocalServer/Services/DanScoreDatumService.cs
@@ -18,32 +18,4 @@ public class DanScoreDatumService : IDanScoreDatumService
             .Include(datum => datum.DanStageScoreData)
             .ToListAsync();
     }
-
-    public async Task<DanScoreDatum?> GetSingleDanScoreDatum(uint baid, uint danId, DanType danType)
-    {
-        return await context.DanScoreData.Include(datum => datum.DanStageScoreData)
-            .FirstOrDefaultAsync(datum => datum.Baid == baid &&
-                                          datum.DanId == danId &&
-                                          datum.DanType == danType);
-    }
-
-    public async Task InsertOrUpdateDanScoreDatum(DanScoreDatum datum)
-    {
-        var existing = await context.DanScoreData.FindAsync(datum.Baid, datum.DanId, datum.DanType);
-        if (existing is null)
-        {
-            context.DanScoreData.Add(datum);
-            await context.SaveChangesAsync();
-            return;
-        }
-
-        context.DanScoreData.Update(datum);
-        await context.SaveChangesAsync();
-    }
-    public void TrackDanStageData(DanStageScoreDatum datum)
-    {
-        context.DanStageScoreData.Add(datum);
-    }
-
-
 }
--- TaikoLocalServer/Services/Interfaces/ISongPlayDatumService.cs
+++ TaikoLocalServer/Services/Interfaces/ISongPlayDatumService.cs
@@ -5,6 +5,4 @@ namespace TaikoLocalServer.Services.Interfaces;
 public interface ISongPlayDatumService
 {
     public Task<List<SongPlayDatum>> GetSongPlayDatumByBaid(uint baid);
-
-    public Task AddSongPlayDatum(SongPlayDatum datum);
 }
