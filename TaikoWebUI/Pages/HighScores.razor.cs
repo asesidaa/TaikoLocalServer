57b0390322a589c00282951bf2014b345222b87b
shiibe <82057235+shiibe@users.noreply.github.com>
2024-03-17 15:46:47 -0400

    Add High Scores page

---
+++ TaikoWebUI/Pages/HighScores.razor.cs
@@ -0,0 +1,81 @@
+﻿using static MudBlazor.Colors;
+using System;
+using Microsoft.JSInterop;
+
+namespace TaikoWebUI.Pages;
+
+public partial class HighScores
+{
+    [Parameter]
+    public int Baid { get; set; }
+
+    private const string IconStyle = "width:25px; height:25px;";
+
+    private SongBestResponse? response;
+    private UserSetting? userSetting;
+
+    private Dictionary<Difficulty, List<SongBestData>> songBestDataMap = new();
+
+    private readonly List<BreadcrumbItem> breadcrumbs = new();
+
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+        response = await Client.GetFromJsonAsync<SongBestResponse>($"api/PlayData/{Baid}");
+        response.ThrowIfNull();
+
+        userSetting = await Client.GetFromJsonAsync<UserSetting>($"api/UserSettings/{Baid}");
+
+        var language = await JSRuntime.InvokeAsync<string>("blazorCulture.get");
+
+        response.SongBestData.ForEach(data =>
+        {
+            var songId = data.SongId;
+            data.Genre = GameDataService.GetMusicGenreBySongId(songId);
+            data.MusicName = GameDataService.GetMusicNameBySongId(songId, string.IsNullOrEmpty(language) ? "ja" : language);
+            data.MusicArtist = GameDataService.GetMusicArtistBySongId(songId, string.IsNullOrEmpty(language) ? "ja" : language);
+        });
+
+        songBestDataMap = response.SongBestData.GroupBy(data => data.Difficulty)
+            .ToDictionary(data => data.Key,
+                          data => data.ToList());
+        foreach (var songBestDataList in songBestDataMap.Values)
+        {
+            songBestDataList.Sort((data1, data2) => GameDataService.GetMusicIndexBySongId(data1.SongId)
+                                      .CompareTo(GameDataService.GetMusicIndexBySongId(data2.SongId)));
+        }
+
+        if (LoginService.IsLoggedIn && !LoginService.IsAdmin)
+        {
+            breadcrumbs.Add(new BreadcrumbItem("Dashboard", href: "/"));
+        }
+        else
+        {
+            breadcrumbs.Add(new BreadcrumbItem("Users", href: "/Users"));
+        };
+        breadcrumbs.Add(new BreadcrumbItem($"{userSetting?.MyDonName}", href: null, disabled: true));
+        breadcrumbs.Add(new BreadcrumbItem("High Scores", href: $"/Users/{Baid}/HighScores", disabled: false));
+    }
+
+    private async Task OnFavoriteToggled(SongBestData data)
+    {
+        var request = new SetFavoriteRequest
+        {
+            Baid = (uint)Baid,
+            IsFavorite = !data.IsFavorite,
+            SongId = data.SongId
+        };
+        var result = await Client.PostAsJsonAsync("api/FavoriteSongs", request);
+        if (result.IsSuccessStatusCode)
+        {
+            data.IsFavorite = !data.IsFavorite;
+        }
+    }
+
+    private static bool IsAiDataPresent(SongBestData data)
+    {
+        var aiData = data.AiSectionBestData;
+
+        return aiData.Count > 0;
+    }
+}
