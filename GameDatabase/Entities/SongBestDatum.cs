35f2448e0026f393434819a294606aedeecb2096
shiibe <82057235+shiibe@users.noreply.github.com>
2024-06-02 19:29:33 -0400

    Add leaderboard API endpoint

--- GameDatabase/Entities/SongBestDatum.cs
+++ GameDatabase/Entities/SongBestDatum.cs
@@ -11,7 +11,12 @@ namespace GameDatabase.Entities
         public uint BestRate { get; set; }
         public CrownType BestCrown { get; set; }
         public ScoreRank BestScoreRank { get; set; }
-
+        public uint GoodCount { get; set; }
+        public uint OkCount { get; set; }
+        public uint MissCount { get; set; }
+        public uint ComboCount { get; set; }
+        public uint HitCount { get; set; }
+        public uint DrumrollCount { get; set; }
         public virtual UserDatum? Ba { get; set; }
     }
 }
---
+++ TaikoLocalServer/Controllers/Api/SongLeaderboardData.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Options;
+using SharedProject.Models;
+using SharedProject.Models.Responses;
+using TaikoLocalServer.Filters;
+using TaikoLocalServer.Settings;
+
+namespace TaikoLocalServer.Controllers.Api;
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class SongLeaderboardData(ISongLeaderboardService songLeaderboardService,IAuthService authService, IOptions<AuthSettings> settings) 
+    : BaseController<SongLeaderboardData>
+{
+    private readonly AuthSettings authSettings = settings.Value;
+
+    [HttpGet("{baid}/{songId}/{difficulty}")]
+    [ServiceFilter(typeof(AuthorizeIfRequiredAttribute))]
+
+    public async Task<ActionResult<SongLeaderboardResponse>> GetSongLeaderboard(uint baid, uint songId, uint difficulty)
+    {
+        if (authSettings.AuthenticationRequired)
+        {
+            var tokenInfo = authService.ExtractTokenInfo(HttpContext);
+            if (tokenInfo is null)
+            {
+                return Unauthorized();
+            }
+
+            if (tokenInfo.Value.baid != baid && !tokenInfo.Value.isAdmin)
+            {
+                return Forbid();
+            }
+        }
+
+        var leaderboard = await songLeaderboardService.GetSongLeaderboard(songId, (Difficulty)difficulty, (int)baid);
+
+        return Ok(new SongLeaderboardResponse
+        {
+            Leaderboard = leaderboard
+        });
+    }
+}
