64347ee9c7423a5e374d4cad46c376e706a38a60
S-Sebb <794194678@qq.com>
2024-11-09 01:49:09 +0800

    Fixed incomplete implementation of ura fumen purchase

--- TaikoLocalServer/Mappers/SongPurchaseMappers.cs
+++ TaikoLocalServer/Mappers/SongPurchaseMappers.cs
@@ -8,8 +8,8 @@ public static partial class SongPurchaseMappers
     public static partial SongPurchaseResponse MapTo3906(CommonSongPurchaseResponse response);
     
     public static partial Models.v3209.SongPurchaseResponse MapTo3209(CommonSongPurchaseResponse response);
-
+    
     public static partial PurchaseSongCommand MapToCommand(SongPurchaseRequest request);
 
-    public static partial PurchaseSongCommand MapToCommand(Models.v3209.SongPurchaseRequest request);
+    public static partial PurchaseSongCommandCN MapToCommand(Models.v3209.SongPurchaseRequest request);
 }
