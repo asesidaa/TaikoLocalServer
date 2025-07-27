bfdbe4caeec79955f1d422efcedc2b69dd179306
Farewell_ <norththelittlefox@gmail.com>
2025-07-27 20:53:18 +0200

    Localize Player Titles
    Allow for localized player titles

--- SharedProject/Models/Title.cs
+++ SharedProject/Models/Title.cs
@@ -6,6 +6,12 @@ public class Title
 
     public string TitleName { get; init; } = string.Empty;
 
+    public string TitleNameEN { get; init; } = string.Empty;
+
+    public string TitleNameCN { get; init; } = string.Empty;
+
+    public string TitleNameKO { get; init; } = string.Empty;
+
     public uint TitleRarity { get; init; }
 
     public override bool Equals(object? obj)
