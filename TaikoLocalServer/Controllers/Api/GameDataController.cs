d73035bdb71ed8b80cf90fe789bb35255989eaf9
asesidaa <1061472754@qq.com>
2026-05-05 00:02:21 +0800

    PR2.16.6: rewrite GameDataController to use IGameDataCatalog

--- TaikoLocalServer/Controllers/Api/GameDataController.cs
+++ TaikoLocalServer/Controllers/Api/GameDataController.cs
@@ -1,93 +1,28 @@
-﻿using Microsoft.Extensions.Options;
 using TaikoLocalServer.Filters;
-using TaikoLocalServer.Infrastructure.Identity.Settings;
 
 namespace TaikoLocalServer.Controllers.Api;
 
 [ApiController]
 [Route("api/[controller]")]
-public class GameDataController(IGameDataCatalog gameDataService, IAuthService authService, 
-    IOptions<AuthSettings> settings) : BaseController<UsersController>
+public class GameDataController(IGameDataCatalog catalog) : BaseController<GameDataController>
 {
-    private readonly AuthSettings authSettings = settings.Value;
-    
     [HttpGet("MusicDetails")]
     [ServiceFilter(typeof(AuthorizeIfRequiredAttribute))]
-    public IActionResult GetMusicDetails()
-    {
-        if (authSettings.AuthenticationRequired)
-        {
-            var tokenInfo = authService.ExtractTokenInfo(HttpContext);
-            if (tokenInfo is null)
-            {
-                return Unauthorized();
-            }
-        }
-        
-        return Ok(gameDataService.GetMusicDetailDictionary());
-    }
-    
+    public IActionResult GetMusicDetails() => Ok(catalog.GetMusicDetailDictionary());
+
     [HttpGet("Costumes")]
     [ServiceFilter(typeof(AuthorizeIfRequiredAttribute))]
-    public IActionResult GetCostumes()
-    {
-        if (authSettings.AuthenticationRequired)
-        {
-            var tokenInfo = authService.ExtractTokenInfo(HttpContext);
-            if (tokenInfo is null)
-            {
-                return Unauthorized();
-            }
-        }
-        
-        return Ok(gameDataService.GetCostumeList());
-    }
-    
+    public IActionResult GetCostumes() => Ok(catalog.GetCostumeList());
+
     [HttpGet("Titles")]
     [ServiceFilter(typeof(AuthorizeIfRequiredAttribute))]
-    public IActionResult GetTitles()
-    {
-        if (authSettings.AuthenticationRequired)
-        {
-            var tokenInfo = authService.ExtractTokenInfo(HttpContext);
-            if (tokenInfo is null)
-            {
-                return Unauthorized();
-            }
-        }
-        
-        return Ok(gameDataService.GetTitleDictionary());
-    }
-    
+    public IActionResult GetTitles() => Ok(catalog.GetTitleDictionary());
+
     [HttpGet("LockedCostumes")]
     [ServiceFilter(typeof(AuthorizeIfRequiredAttribute))]
-    public IActionResult GetLockedCostumes()
-    {
-        if (authSettings.AuthenticationRequired)
-        {
-            var tokenInfo = authService.ExtractTokenInfo(HttpContext);
-            if (tokenInfo is null)
-            {
-                return Unauthorized();
-            }
-        }
+    public IActionResult GetLockedCostumes() => Ok(catalog.GetLockedCostumeDataDictionary());
 
-        return Ok(gameDataService.GetLockedCostumeDataDictionary());
-    }
-    
     [HttpGet("LockedTitles")]
     [ServiceFilter(typeof(AuthorizeIfRequiredAttribute))]
-    public IActionResult GetLockedTitles()
-    {
-        if (authSettings.AuthenticationRequired)
-        {
-            var tokenInfo = authService.ExtractTokenInfo(HttpContext);
-            if (tokenInfo is null)
-            {
-                return Unauthorized();
-            }
-        }
-
-        return Ok(gameDataService.GetLockedTitleDataDictionary());
-    }
+    public IActionResult GetLockedTitles() => Ok(catalog.GetLockedTitleDataDictionary());
 }
