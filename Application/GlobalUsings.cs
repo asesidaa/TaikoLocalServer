f4697a69188c26a330ab33e5bac3b94b6fc68cee
asesidaa <1061472754@qq.com>
2026-05-04 21:41:47 +0800

    PR2.1: scaffold empty TaikoLocalServer.Application project

---
+++ Application/GlobalUsings.cs
@@ -0,0 +1,7 @@
+// Global using directives
+
+global using Mediator;
+global using Microsoft.EntityFrameworkCore;
+global using TaikoLocalServer.Domain.Entities;
+global using TaikoLocalServer.Domain.Enums;
+global using Throw;
