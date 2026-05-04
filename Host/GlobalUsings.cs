30fd65252a1d42cd1346edbc6d364a4accb5753a
asesidaa <1061472754@qq.com>
2026-05-05 02:13:52 +0800

    PR3.6: rename TaikoLocalServer host project to Host (AssemblyName=TaikoLocalServer)

---
+++ Host/GlobalUsings.cs
@@ -0,0 +1,15 @@
+// Global using directives
+
+global using Mediator;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.EntityFrameworkCore;
+global using Serilog;
+global using Swan.Formatters;
+global using TaikoLocalServer.Adapters.AdminApi;
+global using TaikoLocalServer.Adapters.AllnetMucha;
+global using TaikoLocalServer.Adapters.GameProtocol.CnR00;
+global using TaikoLocalServer.Adapters.GameProtocol.WwR08;
+global using TaikoLocalServer.Application;
+global using TaikoLocalServer.Application.Abstractions;
+global using TaikoLocalServer.Domain;
+global using TaikoLocalServer.Infrastructure;
