df180433363081c59090375586630dc7f2a2711e
asesidaa <1061472754@qq.com>
2026-05-12 05:03:19 +0800

    feat(green): add default Green save data helper

---
+++ Application/Common/UserSaveDataGreenExtensions.cs
@@ -0,0 +1,59 @@
+namespace TaikoLocalServer.Application.Common;
+
+public static class UserSaveDataGreenExtensions
+{
+    public static async ValueTask<UserSaveDataGreen> GetOrCreateGreenSaveDataAsync(
+        this ITaikoDbContext context,
+        uint baid,
+        CancellationToken cancellationToken = default)
+    {
+        var saveData = await context.UserSaveDataGreen.FindAsync([baid], cancellationToken);
+        if (saveData is not null)
+        {
+            return saveData;
+        }
+
+        saveData = CreateDefaultGreenSaveData(baid);
+        context.UserSaveDataGreen.Add(saveData);
+        return saveData;
+    }
+
+    public static UserSaveDataGreen CreateDefaultGreenSaveData(uint baid) => new()
+    {
+        Baid = baid,
+        Title = string.Empty,
+        TitleplateId = 0,
+        ColorFace = 0,
+        ColorBody = 1,
+        ColorLimb = 3,
+        Costume1 = 0,
+        Costume2 = 0,
+        Costume3 = 0,
+        Costume4 = 0,
+        Costume5 = 0,
+        CostumeFlg1 = GreenProtocolBytes.CreateFixedBitset([0], GreenProtocolBytes.CostumeFlagBytes),
+        CostumeFlg2 = GreenProtocolBytes.CreateFixedBitset([0], GreenProtocolBytes.CostumeFlagBytes),
+        CostumeFlg3 = GreenProtocolBytes.CreateFixedBitset([0], GreenProtocolBytes.CostumeFlagBytes),
+        CostumeFlg4 = GreenProtocolBytes.CreateFixedBitset([0], GreenProtocolBytes.CostumeFlagBytes),
+        CostumeFlg5 = GreenProtocolBytes.CreateFixedBitset([0], GreenProtocolBytes.CostumeFlagBytes),
+        ToneFlg = GreenProtocolBytes.CreateFixedBitset([0], GreenProtocolBytes.ToneFlagBytes),
+        TitleFlg = new byte[GreenProtocolBytes.TitleFlagBytes],
+        OptionFlg = [],
+        DefaultOptionSetting = new byte[2],
+        DefaultShinSetting = false,
+        DefaultToneSetting = 0,
+        DispDanType = 0,
+        GotDanMax = 0,
+        GotDanFlg = new byte[GreenProtocolBytes.DanFlagBytes],
+        GotDanExtraFlg = new byte[GreenProtocolBytes.DanExtraFlagBytes],
+        DispTaikojukuDan = 0,
+        LastPlayDatetime = DateTime.UnixEpoch,
+        GhostReleaseInfoFlag = new byte[GreenProtocolBytes.GhostReleaseInfoBytes],
+        GhostPlayedSongFlag = new byte[GreenProtocolBytes.GhostPlayedSongBytes],
+        IsAutoCostumeOn = false,
+        IsDevil = false,
+        IsExplain = false,
+        IsChallengeCompe = false,
+        IsTojiru = false
+    };
+}
---
+++ Tests/Green/GreenSaveDataTests.cs
@@ -0,0 +1,24 @@
+namespace TaikoLocalServer.Tests.Green;
+
+public sealed class GreenSaveDataTests
+{
+    [Fact]
+    public void CreateDefaultGreenSaveData_InitializesFixedWidthBytes()
+    {
+        var save = UserSaveDataGreenExtensions.CreateDefaultGreenSaveData(123);
+
+        Assert.Equal((uint)123, save.Baid);
+        Assert.Equal(GreenProtocolBytes.CostumeFlagBytes, save.CostumeFlg1.Length);
+        Assert.Equal(GreenProtocolBytes.CostumeFlagBytes, save.CostumeFlg2.Length);
+        Assert.Equal(GreenProtocolBytes.CostumeFlagBytes, save.CostumeFlg3.Length);
+        Assert.Equal(GreenProtocolBytes.CostumeFlagBytes, save.CostumeFlg4.Length);
+        Assert.Equal(GreenProtocolBytes.CostumeFlagBytes, save.CostumeFlg5.Length);
+        Assert.Equal(GreenProtocolBytes.ToneFlagBytes, save.ToneFlg.Length);
+        Assert.Equal(GreenProtocolBytes.TitleFlagBytes, save.TitleFlg.Length);
+        Assert.Equal(2, save.DefaultOptionSetting.Length);
+        Assert.Equal(GreenProtocolBytes.DanFlagBytes, save.GotDanFlg.Length);
+        Assert.Equal(GreenProtocolBytes.DanExtraFlagBytes, save.GotDanExtraFlg.Length);
+        Assert.Equal(GreenProtocolBytes.GhostReleaseInfoBytes, save.GhostReleaseInfoFlag.Length);
+        Assert.Equal(GreenProtocolBytes.GhostPlayedSongBytes, save.GhostPlayedSongFlag.Length);
+    }
+}
