3fcbc9383a167cd07db701b5e75ff3d01e530c65
asesidaa <1061472754@qq.com>
2026-05-12 04:49:47 +0800

    test: add test project for Green support

---
+++ Tests/Green/GreenProtocolBytesTests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace TaikoLocalServer.Tests.Green;
+
+public sealed class GreenProtocolBytesTests
+{
+    [Fact]
+    public void TestProjectRuns()
+    {
+        Assert.True(true);
+    }
+}
