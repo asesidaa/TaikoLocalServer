3a523b90d299baadb51b45b0c9aa4821cf56193f
asesidaa <1061472754@qq.com>
2026-05-12 01:03:38 +0800

    feat(domain): add UserSaveDataNijiiro entity

---
+++ Domain/Entities/UserSaveDataNijiiro.cs
@@ -0,0 +1,48 @@
+using TaikoLocalServer.Domain.Enums;
+
+namespace TaikoLocalServer.Domain.Entities;
+
+public partial class UserSaveDataNijiiro
+{
+    public uint Baid { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public uint TitlePlateId { get; set; }
+    public List<uint> FavoriteSongsArray { get; set; } = [];
+    public List<uint> ToneFlgArray { get; set; } = [0];
+    public List<uint> TitleFlgArray { get; set; } = [];
+    public List<uint> UnlockedKigurumi { get; set; } = [0];
+    public List<uint> UnlockedHead { get; set; } = [0];
+    public List<uint> UnlockedBody { get; set; } = [0];
+    public List<uint> UnlockedFace { get; set; } = [0];
+    public List<uint> UnlockedPuchi { get; set; } = [0];
+    public uint[] GenericInfoFlgArray { get; set; } = Array.Empty<uint>();
+    public short OptionSetting { get; set; }
+    public int NotesPosition { get; set; }
+    public bool IsVoiceOn { get; set; } = true;
+    public bool IsSkipOn { get; set; }
+    public uint DifficultyPlayedCourse { get; set; }
+    public uint DifficultyPlayedStar { get; set; }
+    public uint DifficultyPlayedSort { get; set; }
+    public uint DifficultySettingCourse { get; set; }
+    public uint DifficultySettingStar { get; set; }
+    public uint DifficultySettingSort { get; set; }
+    public uint SelectedToneId { get; set; }
+    public DateTime LastPlayDatetime { get; set; }
+    public uint LastPlayMode { get; set; }
+    public uint ColorBody { get; set; }
+    public uint ColorFace { get; set; }
+    public uint ColorLimb { get; set; }
+    public uint CurrentKigurumi { get; set; }
+    public uint CurrentHead { get; set; }
+    public uint CurrentBody { get; set; }
+    public uint CurrentFace { get; set; }
+    public uint CurrentPuchi { get; set; }
+    public bool DisplayDan { get; set; }
+    public bool DisplayAchievement { get; set; }
+    public bool DisplaySouUchi { get; set; }
+    public Difficulty AchievementDisplayDifficulty { get; set; }
+    public int AiWinCount { get; set; }
+    public List<uint> UnlockedSongIdList { get; set; } = [];
+    public List<uint> UnlockedUraSongIdList { get; set; } = [];
+    public virtual UserDatum? Ba { get; set; }
+}
