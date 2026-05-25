8c4948f65b57e6a2d227824e5005c8b7c1483496
asesidaa <1061472754@qq.com>
2026-05-26 02:27:47 +0800

    Backfill Green display Dan selection

---
+++ Infrastructure/Persistence/Migrations/20260525182612_BackfillGreenDisplayDanFromGotDanMax.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaikoLocalServer.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class BackfillGreenDisplayDanFromGotDanMax : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                """
+                UPDATE "UserSaveData_Green"
+                SET "DispTaikojukuDan" = CASE
+                    WHEN "GotDanMax" >= 25 THEN 25
+                    ELSE "GotDanMax" + 1
+                END
+                WHERE "GotDanMax" >= 1
+                    AND (
+                        "DispTaikojukuDan" = 0
+                        OR "DispTaikojukuDan" <= "GotDanMax"
+                        OR "DispTaikojukuDan" > 25
+                    );
+                """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Data backfill is intentionally irreversible.
+        }
+    }
+}
