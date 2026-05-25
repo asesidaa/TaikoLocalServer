f4ddca496a71387dfce7b2da87e000ea4937ebd3
asesidaa <1061472754@qq.com>
2026-05-26 01:08:37 +0800

    Backfill Green tojiru default

---
+++ Infrastructure/Persistence/Migrations/20260525170510_BackfillGreenIsTojiruDefault.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaikoLocalServer.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class BackfillGreenIsTojiruDefault : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                """
+                UPDATE "UserSaveData_Green"
+                SET "IsTojiru" = 1;
+                """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Data backfill is intentionally irreversible.
+        }
+    }
+}
